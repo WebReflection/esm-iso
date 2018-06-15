59038ab8e68fc3dc1a2ebd27dabd86580ff8918d
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-06-15 09:07:22 +0200

    shorter, faster, better, blaster

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /*! (c) 2018 Andrea Giammarchi (ISC) */
 ((i, s, o) => {
   const c = i[s];
-  i[s] = (...a) => {
-    if (o.test(a[0])) a[0] = a[0].slice(3);
-    return c.apply(i, a);
-  };
+  i[s] = (...a) => c.apply(i, o.test(a[0]) ? ((a[0] = a[0].slice(3)), a) : a);
 })(
   require('module'), '_resolveFilename',
   /^\/m\//
